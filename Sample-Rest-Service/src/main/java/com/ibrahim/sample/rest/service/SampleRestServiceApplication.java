289ba19204c1c44a19838e53ccf50c3e24b676d7
Ibrahim Chowdhury <ibrahim.chowdhury@bjitgroup.com>
2023-05-22 14:18:00 +0600

    Sample Rest Api Project added

---
+++ Sample-Rest-Service/src/main/java/com/ibrahim/sample/rest/service/SampleRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.sample.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestServiceApplication.class, args);
+	}
+
+}
---
+++ Sample-Rest-Service/src/test/java/com/ibrahim/sample/rest/service/SampleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.sample.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
