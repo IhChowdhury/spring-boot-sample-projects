54ac15373060a087cdee5c78482d3d0b7a604ae4
Ibrahim Chowdhury <ibrahim.chowdhury@bjitgroup.com>
2023-06-06 15:50:44 +0600

    Add Spring boot validation exception handler

--- Sample-Rest-Service/src/main/java/com/ibrahim/sample/rest/service/model/request/NewProductRequest.java
+++ Sample-Rest-Service/src/main/java/com/ibrahim/sample/rest/service/model/request/NewProductRequest.java
@@ -1,13 +1,18 @@
 package com.ibrahim.sample.rest.service.model.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class NewProductRequest {
+    @NotBlank(message = "Product name can not be empty")
     private String productName;
+    @NotBlank(message = "Product Category can not be empty")
     private String productCategory;
+    @Min(0)
     private Double productPrice;
 
 }
